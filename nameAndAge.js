eda06a3fea7573d2d0f0e825a635e56da4901200
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-18 04:51:50 -0300

    Criação da função NameAndAge

--- nameAndAge.js
+++ nameAndAge.js
@@ -1,7 +1,15 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
 const nameAndAge = () => {
-  // escreva seu código aqui
+  const authorInfo = books.map((book) => {
+    const authorName = book.author.name;
+    const authorAge = book.releaseYear - book.author.birthYear;
+    return { author: authorName, age: authorAge };
+  });
+
+  authorInfo.sort((a, b) => a.age - b.age);
+
+  return authorInfo;
 };
 
 module.exports = { nameAndAge };
